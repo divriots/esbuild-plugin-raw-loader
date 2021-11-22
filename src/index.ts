7dcf7f9cce4eb1c109c12e1a14d5dc72d39f5c78
Francois Valdy <francois.valdy@gmail.com>
2021-11-22 14:48:55 +0100

    init

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import fs from "fs";
+import path from "path";
+import type { Plugin } from 'esbuild';
+
+export interface RawLoaderPluginOptions {
+}
+
+export default function firebasePlugin({}: RawLoaderPluginOptions): Plugin {
+  return {
+    name: "esbuild:plugin:raw-loader",
+    setup(build) {
+      build.onResolve(
+        { filter: /^!!raw-loader!/ },
+        ({ path: id, importer }) => ({
+          path: id.slice('!!raw-loader!'.length).startsWith('.')
+            ? path.resolve(
+                path.dirname(importer),
+                id.slice('!!raw-loader!'.length)
+              )
+            : require.resolve(id.slice('!!raw-loader!'.length)),
+          namespace: 'raw',
+        })
+      );
+      build.onLoad({ filter: /./, namespace: 'raw' }, async ({ path }) => {
+        return {
+          contents: `export default ${JSON.stringify(
+            await fs.promises.readFile(path, 'utf-8')
+          )}`,
+          loader: 'js',
+        };
+      });
+    },
+  };
+}
